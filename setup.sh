012a427a7be4235dd577bd15120264e5dad6646a
toballz <42551340+toballz@users.noreply.github.com>
2021-09-27 11:29:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "$1" == "install" ];then
  sudo rm /usr/local/bin/acp
  sudo chmod +x ./a.py
  sudo mv a.py /usr/local/bin/acp
- sudo dos2unix /usr/local/bin/acp
+ sudo dos2unix /usr/local/bin/acp > /dev/null 2>&1
  echo "[!!] Installing DNAMASQ.."
  echo
  sudo apt-get install dnsmasq
