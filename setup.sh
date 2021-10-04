b128964b4968ece3e25ee2c0dbc8c433789c7da4
toballz <42551340+toballz@users.noreply.github.com>
2021-10-04 17:41:34 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,17 +22,12 @@ if [ "$1" == "install" ];then
  echo "[!!] Installing DO2UNIX.."
  sudo apt-get install dos2unix
  echo
- echo "[!!] Installing VLC.."
- sudo apt-get install vlc
- echo
  echo "[!!] Attempt install finished."
  
  sudo rm /usr/local/bin/acp > /dev/null 2>&1
  sudo rm /usr/local/bin/acp_serv.py > /dev/null 2>&1
- sudo rm /usr/local/bin/acp_stay_awake.mp4 > /dev/null 2>&1
  sudo chmod +x ./a.py
  sudo mv a.py /usr/local/bin/acp
- sudo mv acp_stay_awake.mp4 /usr/local/bin/ > /dev/null 2>&1
  sudo mv acp_serv.py /usr/local/bin/ > /dev/null 2>&1
  sudo dos2unix /usr/local/bin/acp > /dev/null 2>&1
 fi
